7b2de6c7b918999f8e9ab3856985637059602cea
Aaron Le <91503688+aaronle03@users.noreply.github.com>
2023-07-03 20:57:23 -0700

    finished homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,15 @@ import './Home.css';
 function Home() {
     return (
         <div className="home">
+            <div className="home-container">
+                <div className="middleTitle">
+                    <h1>Unleash the Power of Fit Fusion</h1>
+                </div>
+                <div className="middleText">
+                    <h1>Where Fitness Meets Transformation</h1>
+                </div>
+                <button type="button" id="enrollButton">Enroll Now →</button>
+            </div>
         </div>
     );
 }
